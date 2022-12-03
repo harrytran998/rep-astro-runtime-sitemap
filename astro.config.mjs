dd6695d5e9a39328e99efc5e8089f0d9efbb315f
harrytran998 <nhattq.coding@gmail.com>
2022-12-03 22:17:40 +0700

    4 files modified, 1 added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,15 @@
 import { defineConfig } from 'astro/config';
+import AstroNode from '@astrojs/node';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server',
+  base: '/ok',
+  server: {
+    port: 4000,
+    host: '0.0.0.0',
+  },
+  adapter: AstroNode({
+    mode: 'standalone',
+  }),
+});
